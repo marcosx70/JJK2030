80fbf2576f8dedf7ebd3eebfb9c8e10c33b0e389
marcosx70 <marcitosx7@gmail.com>
2025-06-17 12:20:01 -0400

    Update InputConfig.lua
    Draft for inputconfig, only temp

--- src/shared/InputConfig.lua
+++ src/shared/InputConfig.lua
@@ -1,1 +1,18 @@
+-- InputConfig.lua
+-- This module returns a dictionary of key bindings for basic player movement and mobility
 
+local InputConfig = {
+    MoveForward = Enum.KeyCode.W,
+    MoveBackward = Enum.KeyCode.S,
+    MoveLeft = Enum.KeyCode.A,
+    MoveRight = Enum.KeyCode.D,
+    
+    Jump = Enum.KeyCode.Space,
+    Dash = Enum.KeyCode.Q, -- You can change this as needed
+    Sprint = Enum.KeyCode.LeftShift,
+    
+    Hover = Enum.KeyCode.E, -- placeholder for cursed-style upgrades
+    ToggleCrouch = Enum.KeyCode.C, -- if crouching gets added
+}
+
+return InputConfig

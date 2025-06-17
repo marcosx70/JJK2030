e3f6b95c5491036909aa4dbddce38422183952be
marcosx70 <marcitosx7@gmail.com>
2025-06-17 16:01:45 -0400

    Update InputConfig.lua

--- src/shared/InputConfig.lua
+++ src/shared/InputConfig.lua
@@ -1,29 +1,54 @@
 -- InputConfig.lua
+-- Configurable key bindings with runtime rebinding support
 
-local UserInputService = game:GetService("UserInputService")
+local InputConfig = {}
 
-local InputConfig = {
-	_bindings = {
-		Jump = Enum.KeyCode.Space,
-		Sprint = Enum.KeyCode.LeftShift,
-		Dash = Enum.KeyCode.Q,
-	},
+-- Default key bindings
+local defaultKeys = {
+	Jump = Enum.KeyCode.Space,
+	Sprint = Enum.KeyCode.LeftShift,
+	Dash = Enum.KeyCode.E,
+	Forward = Enum.KeyCode.W,
+	Backward = Enum.KeyCode.S,
+	Left = Enum.KeyCode.A,
+	Right = Enum.KeyCode.D,
 }
 
-function InputConfig:GetKey(action)
-	return self._bindings[action]
+-- Active key mappings (mutable)
+InputConfig.Keys = table.clone(defaultKeys)
+
+-- Rebind a key at runtime
+function InputConfig.SetKey(actionName, newKeyCode)
+	if not defaultKeys[actionName] then
+		warn("[InputConfig] Invalid action name:", actionName)
+		return
+	end
+	if typeof(newKeyCode) ~= "EnumItem" or newKeyCode.EnumType ~= Enum.KeyCode then
+		warn("[InputConfig] Invalid key code:", newKeyCode)
+		return
+	end
+
+	InputConfig.Keys[actionName] = newKeyCode
 end
 
-function InputConfig:SetKey(action, newKeyCode)
-	if typeof(newKeyCode) == "EnumItem" and newKeyCode.EnumType == Enum.KeyCode then
-		self._bindings[action] = newKeyCode
-	else
-		warn("Invalid keycode for action:", action)
+-- Reset key to default
+function InputConfig.ResetKey(actionName)
+	if defaultKeys[actionName] then
+		InputConfig.Keys[actionName] = defaultKeys[actionName]
 	end
 end
 
-function InputConfig:GetAllBindings()
-	return table.clone(self._bindings)
+-- Reset all keys to default
+function InputConfig.ResetAll()
+	InputConfig.Keys = table.clone(defaultKeys)
 end
+
+-- Get currently assigned key
+function InputConfig.GetKey(actionName)
+	return InputConfig.Keys[actionName] or defaultKeys[actionName]
+end
+
+-- Expose defaults (read-only)
+InputConfig.Defaults = table.clone(defaultKeys)
 
 return InputConfig
